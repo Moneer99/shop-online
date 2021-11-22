0d2fc16ee74ed24618c66801a83ae22acaf52971
Moneer Salmeen <moneerbinoon9@gmail.com>
2021-11-22 22:37:19 +0300

    Modified Template zay shop

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('page.home');
+});
+Route::get('/about', function () {
+    return view('page.about');
+});
+Route::get('/contact', function () {
+    return view('page.contact');
+});
+Route::get('/shop-single', function () {
+    return view('page.shop-single');
+});
+Route::get('/shop', function () {
+    return view('page.shop');
+});
+Route::get('/shop-single', function () {
+    return view('page.shop-single');
+});
